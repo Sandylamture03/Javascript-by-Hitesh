3f2ca27071b9f3a6b3ca318f3493693f983edde3
Sandy_Lamture <sandylamture03@gmail.com>
2025-03-16 17:12:42 +0530

    final chnages done

--- conversionOperation.js
+++ conversionOperation.js
@@ -24,3 +24,34 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 //  console.log(typeof stringNumber);
 //  console.log(stringNumber);
+//  **************************************************Operations ***********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+//console.log(2 + 2);
+//console.log(2 - 2);
+//console.log(2 * 2);
+//console.log(2 ** 2);
+//console.log(2 / 2);
+//console.log(2 % 2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + "2");
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num = num2 = num3 = 2 + 2;
+console.log(num);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
