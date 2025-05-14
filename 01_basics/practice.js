ed73290a70cc38d3a7ae9ad456ba63b9da53b628
Sandy_Lamture <sandylamture03@gmail.com>
2025-05-14 21:58:33 +0530

    all changes done

---
+++ 01_basics/practice.js
@@ -0,0 +1,60 @@
+// let n = 10;
+// for (i = 1; i <= 10; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str = str + `${j} `;
+//   }
+//   console.log(str);
+// }
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+// 1 2 3 4 5 6 7
+// 1 2 3 4 5 6 7 8
+
+// let n = 5;
+// for (i = n; i >= 1; i--) {
+//   let str = " ";
+//   for (let j = 1; j <= i; j++) {
+//     str = str + "* ";
+//   }
+//   console.log(str);
+// }
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+// let n = 5;
+// for (i = 1; i <= n; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str = str + "* ";
+//   }
+//   console.log(str);
+// }
+
+// let m = n - 1;
+// for (i = m; i >= 1; i--) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str = str + "* ";
+//   }
+//   console.log(str);
+// }
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
