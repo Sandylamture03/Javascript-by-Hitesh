60adcab8cd0bc9ecf96fe0de588b04e71c8b9111
Sandy_Lamture <sandylamture03@gmail.com>
2025-05-23 12:50:57 +0530

    all latest chnages done

--- 01_basics/practice.js
+++ 01_basics/practice.js
@@ -58,3 +58,31 @@
 // * * *
 // * *
 // *
+
+// Function currying..
+// with bind method and  with closures
+
+// let multiply = function (x, y) {
+//   console.log(x * y);
+// };
+
+// let multiplyByTwo = multiply.bind(this, 2);
+// multiplyByTwo(3);
+
+// let multiplyByThree = multiply.bind(this, 3);
+// multiplyByThree(5);
+
+// let multiplyByFour = multiply.bind(this, 4);
+// multiplyByFour(5);
+
+let multiply = function (x) {
+  return function (y) {
+    console.log(x * y);
+  };
+};
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(3);
+
+let multiplyByThree = multiply(3);
+multiplyByThree(5);
